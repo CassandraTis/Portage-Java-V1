661028a89af1951c3979fbb1ed017c35017efb1a
CassandraTis <cassandra.muscio@gmail.com>
2015-06-02 14:39:29 +0200

    login fonctionne

--- src/ui/Login.java
+++ src/ui/Login.java
@@ -23,11 +23,13 @@ import princetonPlainsboro.Patient;
  * @author Cassandra
  */
 public class Login extends javax.swing.JFrame {
-    
+
     private String id;
     private String mdpasse;
     private ArrayList<Medecin> mdpsMedecin;
-    
+    private JTextField identifiant;
+    private JTextField mdp;
+
     public Login() {
         //JFrame login = new JFrame();
         setDefaultCloseOperation(EXIT_ON_CLOSE);
@@ -38,68 +40,69 @@ public class Login extends javax.swing.JFrame {
         JPanel connexion = new JPanel();
         connexion.setLayout(new BoxLayout(connexion, Y_AXIS));
         add(connexion);
-        JTextField identifiant = new JTextField();
-        id = identifiant.getText();
-        JTextField mdp = new JTextField();
+        identifiant = new JTextField();
+        System.out.println("identifiant getText :" + identifiant.getText());
+        mdp = new JTextField();
+        System.out.println("mdp getText :" + mdp.getText());
         mdpasse = mdp.getText();
         JButton validation = new JButton("Se connecter");
-        
+
         connexion.add(identifiant);
         connexion.add(mdp);
         connexion.add(validation);
-        
+
         mdpsMedecin = new ArrayList<Medecin>();
         LectureXML pat = new LectureXML("dossiers.xml");
         DossierMedical dm = pat.getDossier();
         for (int i = 0; i < dm.getFiches().size(); i++) {
-            
-            Medecin m = new Medecin(dm.getFiches().get(i).getMedecin().getNom(), dm.getFiches().get(i).getMedecin().getPrenom(), dm.getFiches().get(i).getMedecin().getSpecialite(), dm.getFiches().get(i).getMedecin().getTel(), dm.getFiches().get(i).getMedecin().getIdentifiant(), dm.getFiches().get(i).getMedecin().getMdp());
+
+            Medecin m = new Medecin(dm.getFiches().get(i).getMedecin().getNom(), dm.getFiches().get(i).getMedecin().getPrenom(), dm.getFiches().get(i).getMedecin().getSpecialite(), dm.getFiches().get(i).getMedecin().getTel(), dm.getFiches().get(i).getMedecin().getMdp(), dm.getFiches().get(i).getMedecin().getIdentifiant());
             mdpsMedecin.add(m);
-            
-            
-      
+
         }
-        
-          validation.addActionListener(new java.awt.event.ActionListener() {
-                    public void actionPerformed(java.awt.event.ActionEvent evt) {
-                        validationActionPerformed(evt);
-                    }
-                });    
-        
+
+        validation.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                validationActionPerformed(evt);
+            }
+        });
+
     }
-    
-    public void validationActionPerformed(java.awt.event.ActionEvent evt){
-        System.out.println("OK avant if");
-        new Fenetre().setVisible(true);
-        if (this.checkId()){
-            System.out.println("OK");        
+
+    public void validationActionPerformed(java.awt.event.ActionEvent evt) {
+        id = identifiant.getText();
+        mdpasse = mdp.getText();
+        if (this.checkId()) {
             new Fenetre().setVisible(true);
-                    
-                }
+        }
         this.dispose();
     }
-    
-    public String getIdentifiant() {
-        return id;
-    }
-    
-    public String getMdp() {
-        return mdpasse;
-    }
-    
+    /*
+     public String getIdentifiant() {
+     return id;
+     }
+
+     public String getMdp() {
+     return mdpasse;
+     }
+     */
+
     public boolean checkId() {
         boolean res = false;
         //return res;
 
         for (int i = 0; i < mdpsMedecin.size(); i++) {
-            if (mdpsMedecin.get(i).getIdentifiant().toString().equals(this.id)) {
-                if (mdpsMedecin.get(i).getMdp().toString().equals(this.mdpasse)) {;
+            if (mdpsMedecin.get(i).getIdentifiant().equals(this.id)) {
+                if (mdpsMedecin.get(i).getMdp().equals(this.mdpasse)) {
                     res = true;
+                } else {
+                    res = false;
                 }
-                res = false;
             }
+
         }
         return res;
+
     }
 
     // Pouvoir récupérer tous les mots de passe des médecins issus de l'XML
